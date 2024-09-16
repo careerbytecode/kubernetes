60a6220cd339f39e672e7a1e713c0ecc14a6494d
RajCloudX <32886886+rajcloudx@users.noreply.github.com>
2024-09-16 13:38:10 +0200

    Delete 2-terraform_azure directory

--- 2-terraform_azure/task8_instance_lb/variables.tf
+++
@@ -1,7 +0,0 @@
-# parms file for azure clou
-
-###
-variable "vm_names" {
-  type    = set(string)
-  default = ["vm1", "vm2"]
-}
--- 2-terraform_azure/task9_funtion/05-resource-group.tf
+++
@@ -1,4 +0,0 @@
-resource "azurerm_resource_group" "generic" {
-  name     = "group-generic"
-  location = "North Europe"
-}
